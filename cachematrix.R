d4ee4ab1dc5b4f8ce8cb479fd2155b618cadaeb2
karleb <karl.boggs@gmail.com>
2015-01-24 20:29:47 -0500

    Update cachematrix.R
    cachematrix.R is built on analogy to the provided functions for the cachemean in the assignment discription

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix is a function to bundle a matrix with its calcuated inverse
+## CacheSolve is a function to return the inverse of a matrix
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a special list of a matrix and its inverse, which is a function to
+## set the value of the list, get the value of the list, set the value of the inverse, and get the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) m <<- inverse
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve is a function to return the inverse of the matrix x
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- inverse(data, ...)
+        x$setinverse(m)
+        m
 }
